392132b603944cee7e4aea848e1402417a4f8e9d
fazbat <fazbat@gmail.com>
2016-02-26 13:55:07 -0800

    fixed iso date 0 bug

--- apis.js
+++ apis.js
@@ -9,23 +9,14 @@ module.exports = {
             "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"];
 
         var date = isNaN(iso)? new Date(iso): new Date(Number(iso));
-        var natural = date.getTime()? "" + months[date.getUTCMonth()] + " " +
+        var natural = date.getTime() !== null? "" + months[date.getUTCMonth()] + " " +
                                 date.getUTCDate() +", " + date.getUTCFullYear(): null;
-
-
-
         return JSON.stringify(
             {
                 "unix": date.getTime(),
                 "natural": natural
             }
         )
-    },
-    unixtime: function(iso) {
-        var date = new Date(iso);
-
-        return JSON.stringify(
-            {"unixtime": date.getTime()}
-        )
     }
+
 };
